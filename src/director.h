90eebe7b0fa348b1f72f37edb75f4a7a5a4c237b
ohgodhowdidthisgethere <ohgodhowdidthisgethere>
2015-07-28 00:57:10 +0100

    Actually fix it properly.

--- src/director.h
+++ src/director.h
@@ -127,7 +127,7 @@ private:
   std::string _current_font;
   std::string _current_subfont;
   std::vector<std::string> _subtext;
-  mutable std::vector<sf::Image*> _recent_images;
+  mutable std::vector<uint32_t> _recent_images;
 
   uint32_t _switch_themes;
   std::unique_ptr<Visual> _visual;
