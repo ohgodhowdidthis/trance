ffb5ce6b334e9568976edf75c331c64528e89338
ohgodhowdidthisgethere <ohgodhowdidthisgethere>
2015-12-15 01:39:16 +0000

    Basic program with save and load.

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#ifndef TRANCE_COMMON_H
+#define TRANCE_COMMON_H
+
+#include <string>
+static const std::string SYSTEM_CONFIG_PATH = "system.cfg";
+static const std::string DEFAULT_SESSION_PATH = "default.session";
+
+#endif
